79b53e337c08d99a067cd9d0bfe7860a4a379a1c
Mark <mjc862@mail.usask.ca>
2018-09-28 08:16:13 -0600

    .

--- Assets/Script/Network.cs
+++ Assets/Script/Network.cs
@@ -5,18 +5,29 @@ public class Network : NetworkManager {
 
     public GameObject[] playerFabs;
 
-
+    void Awake() {
+        //playerPrefab = playerFabs[Random.Range(0, 2)];
+    }
+        
+    
     public override void OnStartClient(NetworkClient client) {
         base.OnStartClient(client);
         foreach (GameObject character in playerFabs) {
             ClientScene.RegisterPrefab(character);
         }
+       
     }
 
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId) {
-        GameObject player = GameObject.Instantiate(playerFabs[Random.Range(0,2)]) as GameObject;
-        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
-        return;
+        if (playerControllerId == 0) {
+            GameObject player = GameObject.Instantiate(playerFabs[1]) as GameObject;
+            NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+
+        }
+        else {
+            GameObject player = GameObject.Instantiate(playerFabs[0]) as GameObject;
+            NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+        }
        
     }
 
